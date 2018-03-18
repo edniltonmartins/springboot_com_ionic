43bd22218866ff7fbb5a910abdd7856026c995a1
edniltonmartins <ednilton17@gmail.com>
2018-03-18 17:07:36 -0300

    Projeto Criado

---
+++ src/main/java/com/edniltonmartins/springionic/SpringbootcomionicApplication.java
@@ -0,0 +1,12 @@
+package com.edniltonmartins.springionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcomionicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcomionicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/edniltonmartins/springionic/SpringbootcomionicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.edniltonmartins.springionic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootcomionicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
